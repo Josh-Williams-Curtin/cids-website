9a2044436e3ea94c830f1ef082bda97bf4f726bb
Josh Williams <josh.williams@curtin.edu.au>
2026-07-31 11:41:26 +0800

    github pages config and deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://josh-williams-curtin.github.io',
+	base: '/cids-website',
 	prefetch: {
 		prefetchAll: true,
 	},
